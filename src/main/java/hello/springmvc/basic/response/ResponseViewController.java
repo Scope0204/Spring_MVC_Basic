86f3936268fd16fad535b5c98ed97b5af9b94149
Scope0204 <scope0204@gmail.com>
2022-03-30 11:45:36 +0900

    HTTP 응답-뷰 템플릿+Thymeleaf

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data","hello!");
+        return "response/hello"; // 뷰의 논리적 이름
+    }
+
+    /**
+     * 컨트롤러 경로와 뷰의 논리적 이름이 같은 경우 스프링에서 생략 가능(그다지 권장안함)
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model){
+        model.addAttribute("data","hello!");
+    }
+}
